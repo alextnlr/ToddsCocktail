ad092464707fcb8bda25cdf6481bf40d23ec9fa6
niciavel <alexandre.tonnelier8@etu.univ-lorraine.fr>
2021-12-16 08:08:13 +0100

    Tests requetes bdd

--- install.php
+++ install.php
@@ -8,9 +8,13 @@
 <body>
 <h1>Bases de données MySQL</h1>
 <?php
+/*$servername = 'mysql-toddscocktail.alwaysdata.net';
+$username = '251063';
+$password = 'BlegTotoTata';*/
 $servername = 'localhost';
 $username = 'root';
 $password = 'root';
+$database = 'toddscocktail_boissons';
 
 //On établit la connexion
 $conn = new mysqli($servername, $username, $password);
@@ -18,8 +22,30 @@ $conn = new mysqli($servername, $username, $password);
 //On vérifie la connexion
 if($conn->connect_error){
     die('Erreur : ' .$conn->connect_error);
+} else {
+    echo 'Connexion réussie'.'<br>';
 }
-echo 'Connexion réussie';
+
+$connBd = mysqli_select_db($conn, $database);
+
+if (!$connBd) {
+    if (mysqli_query($conn, "CREATE DATABASE toddscocktail_boissons")) {
+        echo 'Base de donnees crée'.'<br>';
+    } else {
+        echo 'Erreur lors de la création bd'.'<br>';
+    }
+} else {
+    echo "Connecté à la bd".'<br>';
+}
+
+$nomRecette = mysqli_query($conn, "SELECT titre FROM recettes;");
+if ($nomRecette) {
+    while ($row = mysqli_fetch_row($nomRecette)) {
+        echo $row[0] . '<br>';
+    }
+} else {
+    echo 'Erreur lors de la requete'.'<br>';
+}
 ?>
 </body>
 </html>
