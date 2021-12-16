354f53b62f1c1e744e742f50d0a878a0cb6d27e0
timeo <timeo.jacquier@gmail.com>
2021-12-16 09:05:02 +0100

    on peut se co avec le compte bleg mdp bleg

--- index.php
+++ index.php
@@ -1,2 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>BlegCocktail - Accueil</title>
+</head>
+<body>
+<?php include('include/header.php'); ?>
 <?php
-echo ("plop");
+session_start();
+if($_SESSION['username'] !== ""){
+    $user = $_SESSION['username'];
+    // afficher un message
+    echo "Bonjour $user, vous êtes connecté";
+}
+?>
+</body>
+</html>
