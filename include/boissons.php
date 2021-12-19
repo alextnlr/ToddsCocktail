a78cf531d6a87950d5ba4cbeeb04dc52e4a163a2
timeo <timeo.jacquier@hotmail.com>
2021-12-19 12:23:50 +0100

    tentative récupération sur une autre page

--- include/boissons.php
+++ include/boissons.php
@@ -1,1 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>BlegCocktail - Liste</title>
+</head>
+<body>
 <?php
+$servername = 'mysql-toddscocktail.alwaysdata.net';
+$username = '251063';
+$password = 'BlegTotoTata';
+/*$servername = 'localhost';
+$username = 'root';
+$password = 'root';*/
+$database = 'toddscocktail_boissons';
+
+//On établit la connexion
+$conn = new mysqli($servername, $username, $password);
+
+//On vérifie la connexion
+if($conn->connect_error){
+    die('Erreur : ' .$conn->connect_error);
+}
+$nom = str_replace('_', ' ', $_GET['nom']);
+echo $nom;
+$aled = mysqli_query($conn, "SELECT 1 FROM recettes WHERE  = ".$nom);
+?>
+</body>
+</html>
