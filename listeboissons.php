61d231d07920b16850722f07049bc7567cfb47eb
niciavel <alexandre.tonnelier8@etu.univ-lorraine.fr>
2021-12-24 14:36:18 +0100

    Ajout de la recherche par titre + com

--- listeboissons.php
+++ listeboissons.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <title>BlegCocktail - Liste</title>
@@ -25,16 +25,22 @@
 <ul>
     <?php
     $conn = connectDb();
-    if (isset($_GET['k']) && $_GET['k']) {
-        $k = trim($_GET['k']);
-        $keywords = explode(' ', $k);
 
-        $queryRequest = "SELECT r.titre FROM recettes r, ingredients i WHERE r.id_recette = i.id_recette AND ";
+    //S'il y a des choses dans la bar de recherche
+    if (isset($_GET['k']) && $_GET['k']) {
+        $k = trim($_GET['k']); //Enleve les double spaces
+        $keywords = explode(' ', $k); //Separatiojn dans un tableau des mots cles
+
+        //Debut de la requete avec jointure
+        $queryRequest = "SELECT DISTINCT titre FROM recettes LEFT JOIN ingredients i on recettes.id_recette = i.id_recette WHERE ";
         foreach ($keywords as $word) {
-            $queryRequest .= " i.nom_ingredient LIKE '%".$word."%' OR ";
+            //Recherche de chaque mots cle dans le titre ou les ingredients
+            $queryRequest .= " nom_ingredient LIKE '%".$word."%' OR titre LIKE '%".$word."%' OR ";
         }
+        //Supression du dernier OR
         $queryRequest = substr($queryRequest, 0, strlen($queryRequest) - 3);
 
+        //Requete et affichage
         $query = mysqli_query($conn, $queryRequest);
         while ($result = $query->fetch_row()) {
             $changenom = str_replace(' ', '_', $result[0]);
