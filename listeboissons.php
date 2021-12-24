8697ee4ac556abe48d50254bbc3a2c7c49f439b2
niciavel <alexandre.tonnelier8@etu.univ-lorraine.fr>
2021-12-24 14:24:05 +0100

    Ajout de la barre de recherche simple

--- listeboissons.php
+++ listeboissons.php
@@ -12,17 +12,40 @@
     require('include/bddActions.php')
 ?>
 
+<br> <br> <br>
+<form action="" method="get" name="">
+    <table>
+        <tr>
+            <td><input type="text" name="k" placeholder="Search..." autocomplete="off"></td>
+            <td><input type="submit" name="" value="Search"></td>
+        </tr>
+    </table>
+</form>
+
 <ul>
     <?php
-
-    $database = 'toddscocktail_boissons';
     $conn = connectDb();
-    $connBd = mysqli_select_db($conn, $database);
+    if (isset($_GET['k']) && $_GET['k']) {
+        $k = trim($_GET['k']);
+        $keywords = explode(' ', $k);
 
-    $query = mysqli_query($conn, "SELECT titre FROM recettes");
-    while ($result = $query->fetch_row()){
-        $changenom = str_replace(' ', '_', $result[0]);
-        echo "<a href="."boissons.php"."?nom=".$changenom."> <li>".$result[0]."</li></a>";
+        $queryRequest = "SELECT r.titre FROM recettes r, ingredients i WHERE r.id_recette = i.id_recette AND ";
+        foreach ($keywords as $word) {
+            $queryRequest .= " i.nom_ingredient LIKE '%".$word."%' OR ";
+        }
+        $queryRequest = substr($queryRequest, 0, strlen($queryRequest) - 3);
+
+        $query = mysqli_query($conn, $queryRequest);
+        while ($result = $query->fetch_row()) {
+            $changenom = str_replace(' ', '_', $result[0]);
+            echo "<a href=" . "boissons.php" . "?nom=" . $changenom . "> <li>" . $result[0] . "</li></a>";
+        }
+    } else {
+        $query = mysqli_query($conn, "SELECT titre FROM recettes");
+        while ($result = $query->fetch_row()) {
+            $changenom = str_replace(' ', '_', $result[0]);
+            echo "<a href=" . "boissons.php" . "?nom=" . $changenom . "> <li>" . $result[0] . "</li></a>";
+        }
     }
 
     ?>
